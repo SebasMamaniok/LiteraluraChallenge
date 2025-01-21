bdf868e4e94c4af7e0ba53a4e74526043d90964f
SDMok <watsonmzatv@gmail.com>
2025-01-21 19:10:56 -0300

    Subiendo challenge Alura Literalura

---
+++ README.md
@@ -0,0 +1,25 @@
+# Literatura API - Proyecto de Consulta de Libros Gutendex
+
+
+
+## Descripción del Proyecto
+
+Bienvenido al proyecto **Literatura API**, una aplicación de consola desarrollada en Java para la consulta de libros y autores de la API de Gutendex, la cual permite a los usuarios buscar información sobre libros, listar libros por idioma y visualizar detalles sobre autores, incluyendo los que estaban vivos en un año determinado. Un desafio propuesto por Alura Latam en el programa ONE.
+
+
+
+## Funcionalidades 
+
+- `Buscar libros por autor o título`: Permite al usuario buscar información de un libro específico.
+- `Listar libros consultados`: Muestra todos los libros que han sido consultados durante la sesión.
+- `Listar libros por idioma`: Filtra los libros previamente consultados por el idioma seleccionado.
+- `Listar autores`: Muestra todos los autores de los libros consultados.
+- `Listar autores vivos en un año específico`: Muestra los autores que estaban vivos en el año ingresado.
+- `Mostrar estadísticas de libros por idioma`: Despliega la cantidad de libros por idioma según la base de datos almacenada.
+
+
+## API
+
+- [Gutendex API Documentation](https://gutendex.com/docs/)
+
+
---
+++ src/main/java/com/alura/literalura/client/BookApiClient.java
@@ -0,0 +1,43 @@
+package com.alura.literalura.client;
+
+import com.alura.literalura.model.ApiResponse;
+import com.alura.literalura.model.Book;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+public class BookApiClient {
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    // Constructor
+    public BookApiClient() {
+        this.httpClient = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS) // Seguir redirecciones automáticamente
+                .build();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    // Método que se usa para realizar la solicitud a la API
+    public List<Book> fetchBooks(String url) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Error en la solicitud a la API: Código de estado " + response.statusCode());
+        }
+
+        // Con este fragmento convertimos JSON a objetos Book
+        ApiResponse apiResponse = objectMapper.readValue(response.body(), ApiResponse.class);
+        return apiResponse.getResults();
+    }
+}
